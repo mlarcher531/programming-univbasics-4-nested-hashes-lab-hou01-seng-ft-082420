72ccaaaa7b80316318072e71ba9749dc91b9c2dd
Mike Archer <mlarcher531@gmail.com>
2020-07-30 16:32:24 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -65,6 +65,7 @@ def adding_matz
 #   :languages => ["LISP", "C"]
 # }
 # return the entire updated hash
+programmer_hash[:yukihiro_matsumoto] = {known_for: "Ruby", languages: ["LISP", "C"]}
 
 	programmer_hash =
  		{
@@ -82,7 +83,7 @@ def adding_matz
       }
     }
 
-programmer_hash[:yukihiro_matsumoto] = {known_for: "Ruby", languages: ["LISP", "C"]}
+
 end
 
 def changing_alan
