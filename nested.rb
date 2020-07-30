591d607bfd30f089e369022a4c8cfa02088a024b
Mike Archer <mlarcher531@gmail.com>
2020-07-30 16:25:47 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -52,10 +52,10 @@ def dennis_ritchies_language
       },
       :dennis_ritchie => {
         :known_for => "Unix",
-        :languages => ["C"]
+        :languages => "C"
       }
     }
-p programmer_hash[:dennis_ritchie][:languages].to_s
+p programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
